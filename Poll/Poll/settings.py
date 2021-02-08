74e26461ff05ff863a5029663b375af0b223ea97
Rohit Raj <43220855+r0hitraj@users.noreply.github.com>
2021-02-09 00:40:02 +0530

    Update settings.py

--- Poll/Poll/settings.py
+++ Poll/Poll/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '26ki9t%1axa*#tnds#d+w%d(vsx$3#1n=ksnf2#gq1s%w%#2n9'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
